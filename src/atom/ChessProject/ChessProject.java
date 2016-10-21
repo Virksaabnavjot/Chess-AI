3a7280b1f3995aaf89deb9665701b34f5e9529ac
Navjot Singh Virk <virksaabnavjot@gmail.com>
2016-10-21 02:17:04 +0100

    Bishop's Logic Implemented

--- src/atom/ChessProject/ChessProject.java
+++ src/atom/ChessProject/ChessProject.java
@@ -251,7 +251,73 @@ public class ChessProject extends JFrame implements MouseListener, MouseMotionLi
    }
 
    else if(pieceName.contains("Bishop")){
-     validMove = true;
+     Boolean inTheWay = false;
+     int distance = Math.abs(startX-landingX);
+     if(((landingX < 0)||(landingX > 7)) || ((landingY < 0)||(landingY > 7))){
+				validMove = false;
+      }
+      else{
+				validMove = true;
+				if(Math.abs(startX-landingX) == Math.abs(startY-landingY)){
+					if((startX-landingX < 0)&&(startY-landingY < 0)){
+						for(int i = 0; i < distance; i++){
+							if(piecePresent((initialX+(i*75)), initialY+(i*75))){
+								inTheWay = true;
+							}
+						}
+					}
+					else if((startX-landingX < 0)&&(startY-landingY > 0)){
+						for(int i = 0; i < distance; i++){
+							if(piecePresent((initialX+(i*75)),initialY-(i*75))){
+								inTheWay=true;
+							}
+						}
+					}
+					else if((startX-landingX > 0)&&(startY-landingY > 0)){
+						for(int i = 0; i < distance; i++){
+							if(piecePresent((initialX-(i*75)),initialY-(i*75))){
+								inTheWay = true;
+							}
+						}
+					}
+					else if((startX-landingX > 0)&&(startY-landingY < 0 )){
+						for(int i = 0; i < distance; i++){
+							if(piecePresent((initialX-(i*75)), initialY+(i*75))){
+								inTheWay = true;
+							}
+						}
+					}
+          if(inTheWay){
+						validMove = false;
+					}
+					else{
+						if(piecePresent(e.getX(), e.getY())){
+							if(pieceName.contains("White")){
+								if(checkWhiteOponent(e.getX(), e.getY())){
+									validMove = true;
+								}
+								else{
+									validMove = false;
+								}
+							}
+							else{
+								if(checkBlackOponent(e.getX(), e.getY())){
+									validMove = true;
+								}
+								else{
+									validMove = false;
+								}
+							}
+						}
+						else{
+							validMove = true;
+						}
+					}
+				}
+				else{
+					validMove = false;
+				}
+			}
    }
 
    else if(pieceName.equals("BlackPawn")){
